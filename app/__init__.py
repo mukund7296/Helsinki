6b2d7760406c38873e9ec4241063bd45a7f714fc
Mukund Biradar <mukund2015@outlook.com>
2025-05-09 14:47:48 +0200

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+
+mongo = PyMongo()
+
+def create_app():
+    app = Flask(__name__)
+    app.config["MONGO_URI"] = "mongodb://mongo:27017/songs_db"
+    mongo.init_app(app)
+
+    from app.routes import main
+    app.register_blueprint(main)
+
+    return app
